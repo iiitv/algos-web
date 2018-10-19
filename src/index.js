d6a269efd082979eb8e4f56d26402b5ce78eb457
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-19 11:05:34 +0530

    fix gh-pages bug with basename in router props

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div>
       <Route exact path="/" component={App}></Route>
       <Route path="/home" component={HomePage}></Route>
