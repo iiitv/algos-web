6e2d6ff57b0b641451f38c026018b12568b39791
Rohan Singh <rohaniiitv@gmail.com>
2018-12-03 22:32:29 +0530

    fixing test fails

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -7,10 +7,12 @@ class Header extends Component {
       <div>
          <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="true">
             <div className="uk-navbar-left">
+            <BrowserRouter>
                 <ul className="uk-navbar-nav">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/home">Algos</Link></li>
                 </ul>
+            </BrowserRouter>
             </div>
         </nav>
       </div>
