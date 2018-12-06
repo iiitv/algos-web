0976f5ab6c6da38b124b6dcd062e7e3a11c0bb64
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-12-06 08:21:16 +0530

    setup provider store in index.js

--- src/components/welcome/Header.js
+++ src/components/welcome/Header.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
+
+import { loading, loaded } from './../../actions/ghActions';
 import programmer from '../../static/img/programmer.png'
 import './HomeComponent.css'
 
-export default class Header extends Component {
+class Header extends Component {
+
+  componentDidMount() {
+    this.props.loading();
+    // this.props.loaded();
+  }
 
   render() {
     return (
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <div>
+          {
+            this.props.gh.loading ? (
+              <span className="uk-text-danger" data-uk-spinner="ratio: 4.5"></span>
+            ) : null
+          }
           <h2 className='header-title'>IIITV Open Source</h2>
           <h3 className='header-subtitle'>Welcomes You</h3>
         </div>
@@ -17,3 +31,9 @@ export default class Header extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  gh: state.gh
+});
+
+export default connect(mapStateToProps, { loading, loaded })(withRouter(Header));
--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from "react-router";
 import { BrowserRouter, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
+
+import store from './store';
+import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 import HomePage from './components/home/HomePage';
-import registerServiceWorker from './registerServiceWorker';
 import HomeComponent from './components/welcome/HomeComponent.js'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <div>
-      
-      <Switch>
-        {/* New PArrellax component */}
-        {/* <Route exact path="/" component={HomePage}></Route> */}
-        <Route exact path="/" component={HomeComponent}></Route>
-        <Route exact path="/home" component={HomePage}></Route>
-      </Switch>
-    </div>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div>
+
+        <Switch>
+          {/* New PArrellax component */}
+          {/* <Route exact path="/" component={HomePage}></Route> */}
+          <Route exact path="/" component={HomeComponent}></Route>
+          <Route exact path="/home" component={HomePage}></Route>
+        </Switch>
+
+      </div>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
