a9cd80e8971cd610852e37cd22d44e5d058721cc
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-09-01 11:44:52 +0530

    algos fetched

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,33 +1,73 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import './HomePage.css';
 
 class HomePage extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    fetch('https://cors-anywhere.herokuapp.com/https://api.github.com/repos/iiitv/algos/contents', {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: new Headers({
+        'Access-Control-Allow-Origin': '*',
+      }),
+      mode: 'cors',
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          data: data,
+        });
+      });
+  }
+
   render() {
+    const urlList = this.state.data.map(item => {
+      if (item.type === 'dir' && item.name !== '.bin') {
+        return (
+          <a href={item.html_url}>
+            <li key={item.sha}>{item.name}</li>
+          </a>
+        );
+      }
+    });
     return (
       <div className="HomePage">
-          <div className="uk-grid">
-            <div className="uk-width-1-5@s" style={{ background: "#0e0b0bb3", color: "white" }}>
-              <div className="uk-offcanvas-content">
-                <button className="uk-button uk-button-default uk-margin-small-right" type="button" data-uk-toggle="target: #offcanvas-push">Push</button>
+        <div className="uk-grid">
+          <div className="uk-width-1-5@s" style={{ background: '#0e0b0bb3', color: 'white' }}>
+            <div className="uk-offcanvas-content">
+              <button
+                className="uk-button uk-button-default uk-margin-small-right"
+                type="button"
+                data-uk-toggle="target: #offcanvas-push"
+              >
+                Push
+              </button>
 
-                <div id="offcanvas-push" data-uk-offcanvas="mode: push; overlay: true">
-                  <div className="uk-offcanvas-bar">
+              <div id="offcanvas-push" data-uk-offcanvas="mode: push; overlay: true">
+                <div className="uk-offcanvas-bar">
+                  <button className="uk-offcanvas-close" type="button" data-uk-close="true" />
 
-                    <button className="uk-offcanvas-close" type="button" data-uk-close="true"></button>
+                  <h3>Algorithms</h3>
 
-                    <h3>Algorithms</h3>
-
-                    <p>Comming soon.</p>
-                  </div>
+                  <p>Comming soon.</p>
                 </div>
               </div>
             </div>
-            <div className="uk-width-4-5@s">
-              <div style={{ background: "blue" }} className="uk-padding-small">
-                <p>yomo</p>
-              </div>
+          </div>
+          <div className="uk-width-4-5@s">
+            <div style={{ background: 'blue' }} className="uk-padding-small">
+              <p>yomo</p>
             </div>
           </div>
+        </div>
+        <div>
+          <ul>{urlList}</ul>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from "react-router";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { Route } from 'react-router';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
@@ -9,20 +10,18 @@ import store from './store';
 
 import './index.css';
 import HomePage from './components/home/HomePage';
-import HomeComponent from './components/welcome/HomeComponent.js'
+import HomeComponent from './components/welcome/HomeComponent.js';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
-
         <Switch>
           {/* New PArrellax component */}
           {/* <Route exact path="/" component={HomePage}></Route> */}
-          <Route exact path="/" component={HomeComponent}></Route>
-          <Route exact path="/home" component={HomePage}></Route>
+          <Route exact path="/" component={HomeComponent} />
+          <Route exact path="/home" component={HomePage} />
         </Switch>
-
       </div>
     </BrowserRouter>
   </Provider>,
