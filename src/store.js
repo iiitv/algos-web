f6bac71668e02b3ab306ae5660904a77b70e02e2
Creator5923 <nlokhande5923@gmail.com>
2020-09-27 21:36:18 -0700

    space added

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware,compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -13,3 +13,4 @@ const store = createStore(
 );
 
 export default store;
+
