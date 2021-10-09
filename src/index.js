e1a4133c06fd5f0ce7c8189407585ea59443ffb9
marizvi <202051121@iiitvadodara.ac.in>
2021-10-09 16:19:55 +0530

    preloader added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Main from './Main';
 
 // import * as serviceWorker from './serviceWorker';
 import store from './store';
 import './index.css';
-import HomePage from './components/home/HomePage';
-import HomeComponent from './components/welcome/HomeComponent.js';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        {/* New PArrellax component */}
-        {/* <Route exact path="/" component={HomePage}></Route> */}
-        <Route exact path="/" component={HomeComponent}></Route>
-        <Route exact path="/home" component={HomePage}></Route>
-      </Switch>
+      <Main />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
