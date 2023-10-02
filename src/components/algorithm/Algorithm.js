48f356a4235324867e62094871bd967db893fc3f
SteakFisher <miniminter2018p@gmail.com>
2023-10-02 19:32:06 +0530

    Page in between the github transition

---
+++ src/components/algorithm/Algorithm.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import {Link, useParams} from "react-router-dom";
+import axios from 'axios';
+import './Algorithm.css';
+
+const Algorithm = () => {
+  const { name } = useParams();
+  const [data, setData] = useState([]);
+
+  // Get all files under the selected algo and store it into the data variable
+  useEffect(() => {
+    const getDataFromGit = async () => {
+      try {
+        const resp = await axios.get(`https://api.github.com/repos/iiitv/algos/contents/${name}`);
+        console.log(resp.data);
+        setData(resp.data);
+      } catch (e) {
+        console.log(e.response, 'error');
+      }
+    };
+    getDataFromGit();
+  }, []);
+
+  console.log(data)
+
+  let finalRender = data.map((file) => {
+    return (
+      <a key={file.sha} href={`${file.html_url}`}>
+        <li key={file.sha}>{file.name}</li>
+      </a>
+    )
+  })
+  return (
+    <div className="list">
+      <ul>{finalRender}</ul>
+    </div>
+  )
+}
+
+
+export default Algorithm
